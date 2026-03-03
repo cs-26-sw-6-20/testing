aca9efd842307f5633df9d99c98ee2f41782f537
Kresten Laust <krestenlaust@gmail.com>
2026-03-03 13:09:34 +0100

    Update version and code

--- droneshow/src/main/scala/example/Hello.scala
+++ droneshow/src/main/scala/example/Hello.scala
@@ -6,25 +6,34 @@ import org.bytedeco.javacpp.opencv_imgproc._
 import org.bytedeco.javacpp.opencv_highgui._
 
 import org.bytedeco.javacpp.opencv_core.{Mat, Scalar, Point}
+import org.bytedeco.javacpp.opencv_videoio.VideoCapture
 
 object Hello extends Greeting with App {
   println(greeting)
 
-  val image = new Mat(400, 400, CV_8UC3, new Scalar(0, 0, 0, 0))
+  var image = new Mat(600, 600, CV_8UC3, new Scalar(0, 0, 0, 0))
+  val cam = new VideoCapture(0)
 
-  rectangle(
-    image,
-    new Point(100, 100),
-    new Point(300, 300),
-    new Scalar(255, 255, 255, 0),
-    3,
-    LINE_8,
-    0
-  )
+  while true do
+    cam.read(image)
 
-  imshow("Scala OpenCV Hello", image)
+    val edges = new Mat()
+    Canny(image, edges, 100,200)
 
-  waitKey(0)
+
+    rectangle(
+      image,
+      new Point(100, 100),
+      new Point(300, 300),
+      new Scalar(0, 255, 255, 0),
+      3,
+      LINE_8,
+      0
+    )
+
+    imshow("Scala OpenCV Hello", image)
+
+    waitKey(0)
 
   destroyAllWindows()
 }
