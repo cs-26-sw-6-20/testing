86212f72ebc5b2e306790dde5db6cd72c915a584
MalleGJ <amalie@gabejensen.dk>
2026-03-03 13:39:27 +0100

    Edgedetection is working

--- droneshow/src/main/scala/example/Hello.scala
+++ droneshow/src/main/scala/example/Hello.scala
@@ -1,5 +1,6 @@
 package example
 
+// til amalie: C:\Users\Amalie\AppData\Local\Coursier\data\bin\sbt.bat
 import org.bytedeco.javacpp.opencv_core._;
 import org.bytedeco.javacpp.opencv_core._
 import org.bytedeco.javacpp.opencv_imgproc._
@@ -11,17 +12,21 @@ import org.bytedeco.javacpp.opencv_videoio.VideoCapture
 object Hello extends Greeting with App {
   println(greeting)
 
-  var image = new Mat(600, 600, CV_8UC3, new Scalar(0, 0, 0, 0))
+  var image = new Mat(800, 800, CV_8UC3, new Scalar(0, 0, 0, 0))
   val cam = new VideoCapture(0)
 
   while true do
     cam.read(image)
 
     val edges = new Mat()
-    Canny(image, edges, 100,200)
+
+    val gray = new Mat()
+    
+    cvtColor(image, gray, COLOR_BGR2GRAY)
+    Canny(gray, edges, 100, 200)
 
 
-    rectangle(
+/*    rectangle(
       image,
       new Point(100, 100),
       new Point(300, 300),
@@ -29,9 +34,9 @@ object Hello extends Greeting with App {
       3,
       LINE_8,
       0
-    )
+    ) */
 
-    imshow("Scala OpenCV Hello", image)
+    imshow("Scala OpenCV Hello", edges)
 
     waitKey(0)
 
